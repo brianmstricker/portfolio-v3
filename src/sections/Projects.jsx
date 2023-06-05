aa66b59bb3413e9f839fe929df70d69130aeb946
brianmstricker <brianmstricker@gmail.com>
2023-06-05 15:50:55 -0400

    project section updates

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,10 +1,29 @@
 import icongen from "../assets/images/icon-generator.png";
+import { RiExternalLinkFill, RiGithubFill } from "react-icons/ri";
 
 const images = [
-  { name: "Icon Generator", image: icongen },
-  { name: "Icon Generator1", image: icongen },
-  { name: "Icon Generator2", image: icongen },
-  { name: "Icon Generator3", image: icongen },
+  {
+    name: "Icon Generator",
+    image: icongen,
+    text: "React, T3 Stack(Typescript, Prisma, TRPC)",
+    site: "https://icons.brianstricker.com/",
+    code: "https://github.com/brianmstricker/icon-generator",
+  },
+  {
+    name: "Icon Generator1",
+    image: icongen,
+    text: "React, T3 Stack(Typescript, Prisma, TRPC)",
+  },
+  {
+    name: "Icon Generator2",
+    image: icongen,
+    text: "React, T3 Stack(Typescript, Prisma, TRPC)",
+  },
+  {
+    name: "Icon Generator3",
+    image: icongen,
+    text: "React, T3 Stack(Typescript, Prisma, TRPC)",
+  },
 ];
 const Projects = () => {
   return (
@@ -21,11 +40,41 @@ const Projects = () => {
               key={`project-${image.name}`}
             >
               <h2 className="text-xl pb-4">{image.name}</h2>
-              <div>
+              <div className="relative">
                 <img
                   className="w-full h-full lg:w-[44rem] lg:h-[16rem] object-contain"
                   src={image.image}
                 />
+                <div className="absolute top-0 left-0 right-0 bottom-0 opacity-0 hover:opacity-100 hover:bg-black/90 transition-all duration-500">
+                  <div className="flex flex-col justify-between text-lg sm:text-lg lg:text-2xl text-center h-full items-center p-3">
+                    <h3>This was built using:</h3>
+                    <p className="text-red-300">{image.text}</p>
+                    <div className="flex gap-4 text-base">
+                      <div>
+                        <a
+                          className="flex gap-1 cursor-pointer items-center"
+                          href={image.site}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <p>View site</p>
+                          <RiExternalLinkFill size={28} />
+                        </a>
+                      </div>
+                      <div>
+                        <a
+                          className="flex gap-1 cursor-pointer items-center"
+                          href={image.code}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          View code
+                          <RiGithubFill size={28} />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -8,6 +8,7 @@ const images = [
   { name: CSS, style: "shadow-blue-600", text: "CSS" },
   { name: JAVASCRIPT, style: "shadow-yellow-400", text: "Javascript" },
   { name: REACT, style: "shadow-cyan-400", text: "React" },
+  { name: REACT, style: "shadow-cyan-400", text: "React Native" },
 ];
 const Skills = () => {
   return (
@@ -18,10 +19,10 @@ const Skills = () => {
           {images.map((image) => (
             <div
               className={`bg-main px-4 pt-3 pb-[3px] flex flex-col gap-2 rounded-2xl shadow-md ${image.style}`}
-              key={`key-${image.name}`}
+              key={`key-${image.text}`}
             >
               <img
-                className="w-[5.5rem] h-[5.5rem] sm:w-40 sm:h-40 object-contain"
+                className="w-[5.5rem] h-[5.5rem] md:w-40 md:h-40 object-contain"
                 src={image.name}
               ></img>
               <p className="text-center text-lg">{image.text}</p>
